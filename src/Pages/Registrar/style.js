ab5820cdd91b3c1e845cb7c0b7b86d6efde09567
Maria <103320974+mariafiorio@users.noreply.github.com>
2023-06-16 10:18:37 -0300

    Create style.js

---
+++ src/Pages/Registrar/style.js
@@ -0,0 +1,82 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputs: {
+        padding: 10,
+    },
+    inputNome: {
+        height: 30,
+        padding: 5,
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 4,
+        marginBottom: 6,
+    },
+    inputSobrenome: {
+        height: 30,
+        padding: 5,
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 4,
+        marginBottom: 6,
+    },
+    inputEmail: {
+        height: 30,
+        padding: 5,
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 4,
+        marginBottom: 6,
+    },
+    inputSenha: {
+        height: 30,
+        padding: 5,
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 4,
+        marginBottom: 6,
+    },
+    inputConfirmarSenha: {
+        height: 30,
+        padding: 5,
+        color: 'black',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 4,
+    },
+    viewConfirmar:{
+        padding: 5,
+    },
+    btnConfirmar:{
+        height: 30,
+        padding: 5,
+        backgroundColor: 'white',
+        borderRadius: 4,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    txtBtnConfirmar:{
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    viewLink:{
+        flexDirection: 'row',
+        padding: 5,
+    },
+    link:{
+        color: 'black',
+        padding: 5
+    },
+})
+
+export default styles;
